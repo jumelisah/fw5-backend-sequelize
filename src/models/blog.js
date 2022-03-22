015a04b1fa15a3525f09cb01dd3fc2cd1b64f7d9
juumelisa <jumelisa09@gmail.com>
2022-03-22 14:11:56 +0700

    edit blog

--- src/models/blog.js
+++ src/models/blog.js
@@ -2,38 +2,38 @@ const Sequelize = require("sequelize")
 const sequelize = require("../helpers/sequelize")
 
 const Blog = sequelize.define("blog", {
-    title: {
-        type: Sequelize.STRING,
-        validate: {
-            notEmpty: {
-                msg: "Name cannot be empty!"
-            }
-        }
-    },
-    content: {
-        type: Sequelize.STRING,
-        validate: {
-            notEmpty: {
-                msg: "Content cannot be empty"
-            },
-        }
-    },
-    categoryId: {
-        type: Sequelize.INTEGER,
-        validate: {
-            notEmpty: {
-                msg: "Category ID cannot be empty"
-            },
-        }
-    },
-    userId: {
-        type: Sequelize.INTEGER,
-        validate: {
-            notEmpty: {
-                msg: "Category ID cannot be empty"
-            },
-        }
+  title: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: {
+        msg: "Name cannot be empty!"
+      }
     }
+  },
+  content: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: {
+        msg: "Content cannot be empty"
+      },
+    }
+  },
+  categoryId: {
+    type: Sequelize.INTEGER,
+    validate: {
+      notEmpty: {
+        msg: "Category ID cannot be empty"
+      },
+    }
+  },
+  userId: {
+    type: Sequelize.INTEGER,
+    validate: {
+      notEmpty: {
+        msg: "Category ID cannot be empty"
+      },
+    }
+  }
 })
 
 module.exports = Blog
