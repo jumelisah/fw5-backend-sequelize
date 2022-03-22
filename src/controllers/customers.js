98ddba8353537eeff1f9fc11ea1d7512dfd56271
rintosaputro <saputrorinto@gmail.com>
2022-03-22 13:05:40 +0700

    feat: add create, update, getdetail customer

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -21,16 +21,71 @@ exports.getAllCustomers = async (req, res) => {
     }
 }
 
-// exports.getUserById = async (req, res) => {
-//   const {id} = req.params
-//   const customer = await Customers.findByPk(id)
-//   if(id) {
-//     return res.send({
-//       success: true,
-//       message: 'Customer detail',
-//       results: customer
-//     })
-//   } else {
-//     return res.status
-//   }
-// }
+exports.getUserById = async (req, res) => {
+    const {id} = req.params
+    try {
+        const customer = await Customers.findByPk(id)
+        if(id) {
+            return res.send({
+                success: true,
+                message: "Customer detail",
+                results: customer
+            })
+        } else {
+            return res.status(404).send({
+                success: false,
+                message: "Customer undifined"
+            })
+        }
+    } catch (err) {
+        return res.status(500).send({
+            success: false,
+            message: "Error"
+        })
+    }
+}
+
+exports.createCustomer = async (req, res) => {
+    try {
+        const customer = await Customers.create(req.body)
+        return res.send({
+            success: true,
+            message: "Successfully add customers",
+            results: customer
+        })
+    } catch (err) {
+        return res.status(400).send({
+            success: false,
+            message: "Error",
+            results: err.errors.map(e => ({field: e.path, message: e.message}))
+        })
+    }
+}
+
+exports.updateCustomer = async (req, res) => {
+    const {id} = req.params
+    try {
+        const customer = await Customers.findByPk(id)
+        if (customer) {
+            for (let change in req.body) {
+                customer[change] = req.body[change]
+            }
+            await customer.save()
+            return res.send({
+                success: true,
+                message: "Customer successfully updated",
+                results: customer
+            })
+        } else {
+            return res.send({
+                success: false,
+                message: "Customer not found"
+            })
+        }
+    } catch (err) {
+        return res.status(500).send({
+            success: false,
+            message: "Error"
+        })
+    }
+}
--- src/routes/customers.js
+++ src/routes/customers.js
@@ -3,5 +3,7 @@ const customer = require("express").Router()
 const customersController = require("../controllers/customers")
 
 customer.get("/", customersController.getAllCustomers)
+customer.post("/", customersController.createCustomer)
+customer.patch("/:id", customersController.updateCustomer)
 
 module.exports = customer
