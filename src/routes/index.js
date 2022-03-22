f8f525ed01ea337b76280e7a46c4ff81bbcab147
juumelisa <jumelisa09@gmail.com>
2022-03-22 14:01:02 +0700

    edit routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,12 @@
 const routes = require("express").Router()
 
+// routes.use("/users", require("./users"))
+routes.use("/product-categories",require("./productCategories"))
 routes.use("/users", require("./users"))
+routes.use("/customers", require("./customers"))
+routes.use("/transactions", require("./transactions"))
+routes.use("/blogImages", require("./blogImages"))
+routes.use("/product", require("./product"))
 routes.use("/blog", require("./blog"))
 
 module.exports = routes
